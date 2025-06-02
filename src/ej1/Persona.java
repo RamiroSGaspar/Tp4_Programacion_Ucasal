5a5d983ba32e800233ea2e780099386f3be0afbc
RamiroSGaspar <gapsar.sebastian@gmail.com>
2025-06-02 12:30:36 -0300

    .

---
+++ src/ej1/Persona.java
@@ -0,0 +1,140 @@
+package ej1;
+
+/*
+ * UCASAL - PROGRAMACION I
+ * Autor: Ramiro Sebastian Gaspar
+ * ==========================
+ * EJERCICIO 1
+ * Crear una clase “Persona”, con los siguientes atributos:
+ * a. IdPersona: numérico entero
+ * b. Dni: numérico entero
+ * c. Apellido: cadena de texto
+ * d. Nombre: cadena de texto
+ * e. Edad: numérico entero
+ * f. Género: texto
+ * g. Peso: numérico con decimales
+ * h. Altura: numérico con decimales
+ * i. Domicilio: cadena de texto
+ * ==================================================
+ */
+
+public class Persona {
+    // Atributos
+    private int idPersona;
+    private int dni;
+    private String apellido;
+    private String nombre;
+    private int edad;
+    private String genero;
+    private double peso;
+    private double altura;
+    private String domicilio;
+
+    // Constructor vacío
+    public Persona() {
+    }
+
+    // Constructor con todos los atributos
+    public Persona(int idPersona, int dni, String apellido, String nombre,
+                   int edad, String genero, double peso, double altura,
+                   String domicilio) {
+        this.idPersona = idPersona;
+        this.dni = dni;
+        this.apellido = apellido;
+        this.nombre = nombre;
+        this.edad = edad;
+        this.genero = genero;
+        this.peso = peso;
+        this.altura = altura;
+        this.domicilio = domicilio;
+    }
+
+    // Getters y Setters
+    public int getIdPersona() {
+        return idPersona;
+    }
+
+    public void setIdPersona(int idPersona) {
+        this.idPersona = idPersona;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public String getDomicilio() {
+        return domicilio;
+    }
+
+    public void setDomicilio(String domicilio) {
+        this.domicilio = domicilio;
+    }
+
+    // Método toString() para representación del objeto
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "idPersona=" + idPersona +
+                ", dni=" + dni +
+                ", apellido='" + apellido + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", edad=" + edad +
+                ", genero='" + genero + '\'' +
+                ", peso=" + peso +
+                ", altura=" + altura +
+                ", domicilio='" + domicilio + '\'' +
+                '}';
+    }
+}
